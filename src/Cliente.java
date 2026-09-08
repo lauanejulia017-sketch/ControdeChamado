dc1b1321d9b9e2bf2a86845508351dd008c3e485
lauanejulia017-sketch <lauanejulia017@gmail.com>
2026-09-08 15:30:26 -0300

    Adicionar projeto Controle de Chamados

---
+++ src/Cliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlechamados;
+
+public class Cliente extends Usuario {
+    
+    private String cpfCnpj;
+    private String endereco;
+    
+    public String getCpfCnpj(){
+        return cpfCnpj;
+        
+    }
+    
+}
---
+++ src/controlechamados/Atendimento.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlechamados;
+
+import java.time.LocalDateTime;
+
+public class Atendimento {
+
+    private Long id;
+    private LocalDateTime dataHora;
+    private String descricao;
+    private Tecnico realizadoPor;
+
+    public void registrar() {
+
+    }
+}
